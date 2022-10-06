917eeddd7ea04cdab623084e800a5199f2192ca9
Marty Miguel <martymiguel@gmail.com>
2022-10-06 22:33:56 +0800

    working on file upload

--- src/components/FileUpload.js
+++ src/components/FileUpload.js
@@ -2,12 +2,14 @@ import { Form } from 'bootstrap-4-react';
 
 export default function FileUpload() {
     return (
-       <Form id="form-file-upload">
-       		<label id="label-file-upload" htmlFor="input-file-upload">
-       		    <div>
-       		        <p>Drag and Drop to upload files</p>
-       		    </div> 
-       		</label>
-       </Form>
+       <div id="file-upload-section">
+	       <Form id="form-file-upload">
+	       		<label id="label-file-upload" htmlFor="input-file-upload">
+	       		    <div>
+	       		        <p>Drag and Drop to upload files</p>
+	       		    </div> 
+	       		</label>
+	       </Form>
+	    </div>
     );
 };
