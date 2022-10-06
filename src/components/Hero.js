6e8544d14bb79b6d364918e13ff261f47311929e
Marty Miguel <martymiguel@gmail.com>
2022-10-06 21:12:25 +0800

    done with header and hero section

---
+++ src/components/Hero.js
@@ -0,0 +1,12 @@
+import { Jumbotron, Lead  } from 'bootstrap-4-react';
+
+export default function Hero() {
+    return (
+      <Jumbotron id="hero">
+            <Lead id="hero-title">CompanyNet</Lead>  
+            <div id="hero-image">
+                  
+            </div>  
+      </Jumbotron>
+    )
+}
---
+++ src/components/NavBar.js
@@ -0,0 +1,22 @@
+import { Navbar, Nav } from 'bootstrap-4-react';
+
+export default function NavBar() {
+   return(
+   		<Navbar id="navigation" expand="lg" light bg="light">
+   			<Navbar.Toggler target="#navbar-content" />
+   			<Navbar.Collapse id="navbar-content">
+	   		    <Navbar.Nav ml="auto">
+		            <Nav.Item>
+		              <Nav.Link href="#">Home</Nav.Link>
+		            </Nav.Item>
+		            <Nav.Item>
+		              <Nav.Link href="#">About Us</Nav.Link>
+		            </Nav.Item>
+		            <Nav.Item>
+		              <Nav.Link href="#">Contact</Nav.Link>
+		            </Nav.Item>
+	            </Navbar.Nav>
+	        </Navbar.Collapse>
+   		</Navbar>
+   );
+};
