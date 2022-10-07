71bb94a0be35b51f2fc489eff56705e78b172e6e
Marty Miguel <martymiguel@gmail.com>
2022-10-07 19:48:23 +0800

    done cleaning up

--- src/components/footer/OurLocation.js
+++ src/components/footer/OurLocation.js
@@ -5,7 +5,7 @@ export default function OurLocation() {
 		    <h2 id="contact-label">Our Location</h2>
 		    <div className="mapouter">
 		        <div className="gmap_canvas">
-		             <iframe width="600" height="500" id="gmap_canvas" src="https://maps.google.com/maps?q=Level%201,%201%20Ulster%20Lane%20Brisbane%20QLD%204000%20Australia&t=k&z=19&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0">
+		             <iframe width="600" height="500" title="location_map" id="gmap_canvas" src="https://maps.google.com/maps?q=Level%201,%201%20Ulster%20Lane%20Brisbane%20QLD%204000%20Australia&t=k&z=19&ie=UTF8&iwloc=&output=embed" frameBorder="0" scrolling="no" marginHeight="0" marginWidth="0">
 		             </iframe> 
 		        </div>
 		    </div>
